71bae5fdd27e98aed2257619ebc37bd31ee0cbd3
jcabrigo <jcabrigo@gmail.com>
2025-08-24 10:58:54 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Aqui almacenamos los nombres
+const amigos = [];
+
+// Agregamos un nombre
+function agregarAmigo() {
+  const input = document.getElementById("amigo");
+  let nombre = input.value;
+
+  // Validacion espacio vacio
+  if (nombre.length === "") {
+    alert("El nombre no puede estar vacio.");
+    input.value = "";
+    return;
+  }
+
+  amigos.push(nombre);
+  mostrarLista();
+  input.value = "";
+}
+
+// Aqui mostramos la lista de amigos
+function mostrarLista() {
+  const lista = document.getElementById("listaAmigos");
+  lista.innerHTML = "";
+
+  amigos.forEach((amigo) => {
+    const li = document.createElement("li");
+    li.textContent = amigo;
+    lista.appendChild(li);
+  });
+}
+
+// Sorteo
+function sortearAmigo() {
+  const resultado = document.getElementById("resultado");
+  resultado.innerHTML = "";
+
+  if (amigos.length === 0) {
+    alert("Se debe ingresar al menos un nombre para el sorteo.");
+    return;
+  }
+
+  const indice = Math.floor(Math.random() * amigos.length);
+  const seleccionado = amigos[indice];
+
+  const li = document.createElement("li");
+  li.textContent = `El amigo secreto es: ${seleccionado}`;
+  resultado.appendChild(li);
+}
